eb65b8cbd248da2518bcf98e72d51888bc52e06f
ansh <sharma.16ansh@gmail.com>
2021-05-07 00:06:40 +0530

    updated

---
+++ project 5.js
@@ -0,0 +1,16 @@
+let onei = document.getElementById("onei");
+let twoi = document.getElementById("twoi");
+let threei = document.getElementById("threei");
+let fouri = document.getElementById("fouri");
+let fivei = document.getElementById("fivei");
+
+if (document.getElementById("slider").style.left = 0 % ) {
+    onei.classList.add("active-indicator");
+}
+
+
+
+
+onei.onclick = function() {
+
+}
